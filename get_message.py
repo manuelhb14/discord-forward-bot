fd01fa0434d4bcded5060a0d48e94550dec3b9f9
Manuel Hernandez <48745085+manuelhb14@users.noreply.github.com>
2021-06-07 23:10:09 -0500

    add email and password to inputs

--- get_message.py
+++ get_message.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import keys
 
 driver = webdriver.Firefox(executable_path="./geckodriver.exe")
 driver.get("http://www.discord.com/login")
-
+element = driver.find_element_by_name("email")
+element.send_keys(keys.email)
+element = driver.find_element_by_name("password")
+element.send_keys(keys.password)
 time.sleep(5)
 driver.close()
