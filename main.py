84bb25ec217431a790c1a8a41e0a5958b5223cc8
Manuel Hernandez <48745085+manuelhb14@users.noreply.github.com>
2021-06-21 01:25:43 -0500

    get image links and text from last discord message

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import startup
+from copy import Error
+import setup
 import time
 import message
 
-driver = startup.login()
-startup.select_group(6, driver)
-startup.select_channel(2, driver)
+driver = setup.login()
+setup.select_group(6, driver)
+# setup.select_channel(2, driver)
+setup.select_channel(1, driver)
 
 last_message = message.get_message(driver)
+message.get_data(driver, last_message)
 
 while True:
     try:
         time.sleep(3)
         new_message = message.get_message(driver)
         if new_message != last_message:
-            print(new_message.text)
+            message.get_data(driver, new_message)
             last_message = new_message
-    except KeyboardInterrupt:
-        print("Logging out")
+    except Exception as e:
+        pass
